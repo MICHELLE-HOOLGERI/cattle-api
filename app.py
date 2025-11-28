79b178bf25d0c347b9dd51ed4fe31fdb5755e1f1
MICHELLE-HOOLGERI <01fe23bci042@kletech.ac.in>
2025-11-28 20:34:31 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, jsonify
+from models.cattle_model import CattleModel
+
+app = Flask(__name__)
+model = CattleModel()
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    if "file" not in request.files:
+        return jsonify({"error": "No file uploaded"}), 400
+
+    file = request.files["file"]
+    file_path = "temp.jpg"
+    file.save(file_path)
+
+    pred, conf = model.predict(file_path)
+
+    return jsonify({
+        "model": "cattle",
+        "prediction": pred,
+        "confidence": conf
+    })
+
+@app.route("/")
+def home():
+    return jsonify({"message": "Cattle Model API is running"})
+
+if __name__ == "__main__":
+    app.run(debug=True)
