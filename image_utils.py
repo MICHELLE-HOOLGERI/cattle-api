2461087fe83a1e8ef3197ddda9b5adaa7e3c41de
MICHELLE-HOOLGERI <01fe23bci042@kletech.ac.in>
2025-11-28 23:40:27 +0530

    Update image_utils.py

--- image_utils.py
+++ image_utils.py
@@ -1,7 +1,11 @@
-import numpy as np
 from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+import numpy as np
 
 def preprocess_image(img_path, img_size):
     img = image.load_img(img_path, target_size=(img_size, img_size))
-    img_array = image.img_to_array(img) / 255.0
-    return np.expand_dims(img_array, axis=0)
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array = preprocess_input(img_array)  # 🔥 Critical line
+    return img_array
+
